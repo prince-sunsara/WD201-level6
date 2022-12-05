7b60de866c475645b06cd293eae731bdecb86c4d
prince-sunsara <princesunasara977@gmail.com>
2022-12-05 11:03:37 +0530

    files updated

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-    response.send("Hello worlds!")
+    response.send("Go to localhost:3000/todos")
+})
+
+app.get("/todos", (request, response) => {
+    // response.send("Hello Developers!!")
+    console.log("Todo list")
+
+})
+
+app.post("/todos", (request, response) => {
+    console.log("Creating a Todo", request.body)
+
+})
+
+app.put("/todos/:id/markAsCompleted", (request,response) => {
+    console.lof("We have to update a new todo with ID:", request.params.id)
+})
+
+app.delete("/todos/:id", (request, response) => {
+    console.log("Delete a todo by ID: ", request.params.id  )
 })
 
 module.exports = app;
