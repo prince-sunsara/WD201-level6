132d33686d91955c94b1bcb5fd28a752c320b535
prince-sunsara <princesunasara977@gmail.com>
2022-12-05 11:59:02 +0530

    files updated

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
-    response.send("Go to localhost:3000/todos")
-})
+  response.send("Go to localhost:3000/todos");
+});
 
 app.get("/todos", (request, response) => {
-    // response.send("Hello Developers!!")
-    console.log("Todo list")
+  response.send("Hello Developers!!");
+  //   console.log("Todo list");
+});
 
-})
+app.post("/todos", async (request, response) => {
+  console.log("Creating a Todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
-app.post("/todos", (request, response) => {
-    console.log("Creating a Todo", request.body)
-
-})
-
-app.put("/todos/:id/markAsCompleted", (request,response) => {
-    console.lof("We have to update a new todo with ID:", request.params.id)
-})
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to update a new todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
 app.delete("/todos/:id", (request, response) => {
-    console.log("Delete a todo by ID: ", request.params.id  )
-})
+  console.log("Delete a todo by ID: ", request.params.id);
+});
 
 module.exports = app;
